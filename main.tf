8b56cf643e28d5abb23519c3844435773e6f1d8b
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-04 15:56:05 +0530

    actions

--- main.tf
+++ main.tf
@@ -29,3 +29,39 @@ resource "aws_subnet" "public" {
     var.public_subnet_tags
   )
 }
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)  # 2 public_subnet_cidrs
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index] # 2
+  availability_zone = local.az_names[count.index]
+  # map_public_ip_on_launch = true # By default it is false, to enable we use true orelse we won't mention(To enable public ip...
+
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-private_us-east-1a, roboshop-dev-private_us-east-1b
+    {
+      Name  = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    },
+    var.private_subnet_tags
+  )
+}
+
+# Database Subnets
+resource "aws_subnet" "database" {
+  count             = length(var.database_subnet_cidrs) # 2 public_subnet_cidrs
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.database_subnet_cidrs[count.index] # 2
+  availability_zone = local.az_names[count.index]
+  # map_public_ip_on_launch = true # By default it is false, to enable we use true orelse we won't mention(To enable public ip...
+
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-database-us-east-1a, roboshop-dev-database-us-east-1b
+    {
+      Name  = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    },
+    var.database_subnet_tags
+  )
+}
--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "public_subnet_tags" {
   default     = {}
 }
 
-/* variable "private_subnet_cidrs" {
+variable "private_subnet_cidrs" {
   type        = list
   default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
@@ -51,4 +51,4 @@ variable "database_subnet_cidrs" {
 variable "database_subnet_tags" {
   type        = map
   default     = {}
-} */
+}
