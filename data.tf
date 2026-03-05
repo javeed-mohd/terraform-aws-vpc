ddfbcb8a1c254373b46fd5ba8e5c2067e2548eab
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-05 19:03:43 +0530

    actions

--- data.tf
+++ data.tf
@@ -7,3 +7,12 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default" {
   default = true
 }
+
+# To get aws_route_table by default, we use the aws_route_table data source
+data "aws_route_table" "default" {
+  vpc_id   = data.aws_vpc.default.id 
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
--- peering.tf
+++ peering.tf
@@ -28,3 +28,19 @@ resource "aws_vpc_peering_connection" "default" {
     }
   )
 }
+
+# Public Route Table Peering Connection
+resource "aws_route" "public_peering" {
+  count                     = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id 
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+# Default Peering Connection
+resource "aws_route" "default_peering" {
+  count                     = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.default.id 
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
