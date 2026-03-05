6a48e46e6aef40ff2cb4ca10c5bac93054da6943
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-05 16:56:42 +0530

    actions

--- main.tf
+++ main.tf
@@ -107,3 +107,56 @@ resource "aws_route_table" "database" {
     var.database_route_table_tags # For users
   )
 }
+
+# Public Route Table(Internet Gateway)
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.main.id
+}
+
+# Elastic IP Creation
+resource "aws_eip" "nat" {
+  domain                    = "vpc"
+
+  tags  = merge(
+    local.common_tags,
+    {
+      Name  = "${var.project}-${var.environment}-nat"
+    },
+    var.eip_tags
+  )
+}
+
+# AWS NAT-Gateway
+# To create NAT Gateway, we need Elastic IP which is a static IP which does not change after start/stop of the instance...
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id # We are creating this in us-east-1a AZ (public[1] -> us-east-1b)
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name  = "${var.project}-${var.environment}"
+    },
+    var.nat_gateway_tags
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main] # We have to add it explicitly to connect internet gateway through nat gateway...
+}
+
+# Private Route Table(NAT Gateway -> Outgoing traffic)
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id # 0.0.0.0/0 NAT -> NAT(Network Address Translation) Gateway to connect public subnet internally.
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.main.id # Specific for NAT Gateway
+}
+
+# Database Route Table(NAT Gateway -> Outgoing traffic)
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id # 0.0.0.0/0 NAT -> NAT(Network Address Translation) Gateway to connect public subnet internally.
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.main.id # Specific for NAT Gateway
+}
