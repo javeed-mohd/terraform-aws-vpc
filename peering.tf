2e4afffe66605334d0e65255f168b9fc978ce61e
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-05 19:09:59 +0530

    actions

--- peering.tf
+++ peering.tf
@@ -37,10 +37,26 @@ resource "aws_route" "public_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
+# Private Route Table Peering Connection
+resource "aws_route" "private_peering" {
+  count                     = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id 
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+# Database Route Table Peering Connection
+resource "aws_route" "database_peering" {
+  count                     = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id 
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
 # Default Peering Connection
 resource "aws_route" "default_peering" {
   count                     = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.default.id 
+  route_table_id            = data.aws_route_table.default.id 
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
