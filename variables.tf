437a86e9f4ac522122e5a141ee5e0780b6dd1545
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-06 13:34:21 +0530

    actions

--- variables.tf
+++ variables.tf
@@ -5,6 +5,11 @@ variable "project" {
 
 variable "environment" {
   type      = string
+  # Optional
+  validation {
+        condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
+        error_message = "Environments should be one of dev, qa, uat or prod"
+    }
 }
 
 variable "vpc_cidr" {
