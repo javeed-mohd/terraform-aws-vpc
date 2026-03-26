589176542458eabe1b4a33530ae4f04405f93e3a
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-26 23:50:27 +0530

    actions

--- data.tf
+++ data.tf
@@ -1,15 +1,15 @@
 # Data sources are used to query and fetch existing information from your provider like AWS, so you can use that data in your Terraform configuration.
-# To get aws_availability_zones by default, we use the aws_availability_zones data source
+# To get aws_availability_zones by default, we use the aws_availability_zones data source.
 data "aws_availability_zones" "available" {
   state  = "available"
 }
 
-# To get aws_vpc by default, we use the aws_vpc data source
+# To get aws_vpc by default, we use the aws_vpc data source.
 data "aws_vpc" "default" {
   default = true
 }
 
-# To get aws_route_table by default, we use the aws_route_table data source
+# To get aws_route_table by default, we use the aws_route_table data source.
 data "aws_route_table" "default" {
   vpc_id   = data.aws_vpc.default.id 
   filter {
